cec1513f7c600025d44bb4db6775fca19511a757
x0tuzi <x0tuzi@users.noreply.github.com>
2026-07-14 00:06:32 +0800

    test: v0.0.0.1 for update testing

--- bilibili_sniffer.py
+++ bilibili_sniffer.py
@@ -79,7 +79,7 @@ else:
     HISTORY_FILE = os.path.expanduser('~/.bili_sniffer_history')
     DEFAULT_DL_DIR = os.path.expanduser('~/bilibili_downloads')
 
-VERSION         = "1.1.0"
+VERSION = "0.0.0.1"
 GITHUB_REPO     = "x0tuzi/bili-sniffer"
 GITHUB_API      = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 GITHUB_RAW      = f"https://raw.githubusercontent.com/{GITHUB_REPO}/main"
